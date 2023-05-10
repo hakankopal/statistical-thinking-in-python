857c78ced6a4576e52c3bfbabf4bccbc7c1f0fa0
hakankopal <58104324+hakankopal@users.noreply.github.com>
2023-05-10 11:24:46 +0000

    new scripts were added.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -4,3 +4,8 @@ def ecdf(data):
     x = np.sort(data)
     y = np.arange(1, len(x)+1) / len(x)
     return x, y
+
+def bootstrap_replicate_1d(data, func):
+    """Generate bootstrap replicate of 1D data."""
+    bs_sample = np.random.choice(data, len(data))
+    return func(bs_sample)
